2ddfb01fdef1701b725cd04e10c2b79e6cbb2f30
madanmagar <magarmadan2001@gmail.com>
2022-10-16 14:26:59 +0545

    fixed some bugs

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const button = document.querySelector('.btn');
-const hexaColor = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+const hexaColor = ["F",1,"B",3,"E",5,6,7,8,"A",9,2,"C",4,"D",0];
 const colorCodeInHexa = document.querySelector('.color');
 function hexGenerator() {
     return Math.floor(Math.random() * hexaColor.length);
@@ -11,6 +11,7 @@ function hexaColorGenerator() {
         hexaColorCode = hexaColorCode + hexaColor[hexGenerator()]
     }
     document.body.style.backgroundColor = hexaColorCode;
+    document.querySelector('.color').innerText = "Background Color : " + hexaColorCode;
 }
 
 button.addEventListener('click',hexaColorGenerator);
@@ -19,4 +20,4 @@ document.body.addEventListener('keypress', function(e) {
     if (e.keyCode == 32) {
         hexaColorGenerator()
     }
-})
+});
